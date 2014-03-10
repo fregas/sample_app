8ac64734d608e8d5b937097ea4410ffb691a85c9
Robert Landreaux <rcl2288@yahoo.com>
2014-03-10 18:09:26 -0500

    Ch. 7 Exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -31,6 +31,13 @@ describe "User pages" do
       end
     end
 
+    describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -41,6 +48,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
