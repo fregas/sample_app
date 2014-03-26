2c2dd2b3af8ef7d565044e0fb6ff0580e75fbc95
Robert Landreaux <rcl2288@yahoo.com>
2014-03-25 20:19:56 -0500

    Stuck on Ch. 9

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -95,14 +95,14 @@ describe User do
     let(:found_user) { User.find_by_email(@user.email) }
 
     describe "with valid password" do
-      it { should == found_user.authenticate(@user.password) }
+      it { should eq found_user.authenticate(@user.password) }
     end
 
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 
-      it { should_not == user_for_invalid_password }
+      it { should_not eq user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end 
     end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,25 +25,22 @@ describe "Authentication" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
     end
-end
 
 describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_title(user.name) }
       it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Settings',    href: edit_user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) } 
+      it { should_not have_link('Sign in', href: signin_path) }
 
         describe "followed by signout" do
-        before { click_link "Sign out" }
+        before { click_link 'Sign out' }
         it { should have_link('Sign in') }
    end
-  end
  end
+end
+  end
 end
